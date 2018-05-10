8d2a71d5ded0b0b44ed50bdcc82ef384cec6c507
amirh <amir74heidari@gmail.com>
2018-05-10 21:11:02 +0200

    Revert "reconfiguring services adding the answers and questions service + authentification"
    This reverts commit e996f8d2ab89feeef19a6b84963f9acb4583479b.

--- src/app/core/services/profiles.service.ts
+++ src/app/core/services/profiles.service.ts
@@ -15,4 +15,13 @@ export class ProfilesService {
     return this.apiService.get('/profiles/' + username)
       .pipe(map((data: {profile: Profile}) => data.profile));
   }
+
+  follow(username: string): Observable<Profile> {
+    return this.apiService.post('/profiles/' + username + '/follow');
+  }
+
+  unfollow(username: string): Observable<Profile> {
+    return this.apiService.delete('/profiles/' + username + '/follow');
+  }
+
 }
